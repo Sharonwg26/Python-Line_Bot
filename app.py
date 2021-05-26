a07eda41470aeaed27cd39aeb9a15f844077c323
Sharon <49950909+Sharonwg26@users.noreply.github.com>
2021-05-26 15:51:01 +0800

    Update app.py
    台台台

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ line_bot_api = LineBotApi('OQqOlMprRqeiXhBHSymZ45X+i7WF74swYPh3nYpgcrVezoVQQ726E
 # Channel Secret
 handler = WebhookHandler('69a5b0ff850e58d346a7695507d98177')
 
-cities = ['基隆市','嘉義市','臺北市','嘉義縣','新北市','臺南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','苗栗縣','花蓮縣','臺中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
+cities = ['基隆市','嘉義市','臺北市','台北市','嘉義縣','新北市','臺南市','台南市','桃園縣','高雄市','新竹市','屏東縣','新竹縣','臺東縣','台東縣','苗栗縣','花蓮縣','臺中市','台中市','宜蘭縣','彰化縣','澎湖縣','南投縣','金門縣','雲林縣','連江縣']
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
